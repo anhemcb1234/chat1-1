e4c15fb004a9df9723d08b5553a2d8c75f8db608
HOANG TRUNG TIEN <hoangtrungtien99@gmail.com>
2022-04-27 15:49:18 +0700

    done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import {auth} from "../../firebase";
-
+import {useNavigate} from 'react-router-dom'
 function Home() {
+    let navigate = useNavigate();
     useEffect(() => {
         auth.onAuthStateChanged((user) => {
             if (user) {
@@ -9,10 +10,16 @@ function Home() {
             }
           })
     },[])
+    const _logOut = () => {
+        auth.signOut();
+        sessionStorage.removeItem('user');
+        navigate('/')
+    }
     return (
         <div className="py-10 h-screen bg-gray-300 px-2">
     <div className="max-w-md mx-auto bg-gray-100 shadow-lg rounded-lg overflow-hidden md:max-w-lg">
         <div className="md:flex">
+        <button className='px-3 my-5 float-right py-2 text-sm text-blue-100 bg-red-600 rounded' onClick={_logOut}>Đăng xuất</button>
             <div className="w-full p-4">
                 <div className="relative"> <input type="text" className="w-full h-12 rounded focus:outline-none px-3 focus:shadow-md" placeholder="Search..."/>  </div>
                 <ul>
