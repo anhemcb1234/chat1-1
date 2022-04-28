e09d4d8f2194f7ab311616a8d41dc63fb89c7656
HOANG TRUNG TIEN <hoangtrungtien99@gmail.com>
2022-04-28 10:17:44 +0700

    Fix css on page chatbox

--- src/pages/ChatBox.js
+++ src/pages/ChatBox.js
@@ -14,6 +14,7 @@ export default function ChatBox() {
   const [user, SetUser] = useState([]);
   const [idUserTwo, SetIdUserTwo] = useState('');
   const [dataSort, setDataSort] = useState([]);
+  const [userTwo, setUserTwo] = useState([]);
   let unsub = null
   //
   useEffect(() => {
@@ -56,6 +57,7 @@ export default function ChatBox() {
     SetUser(users);
     SetIdUserTwo(user?.find(x => x.id === userIdTwo)?.id_user)
     setDataSort()
+    getInfoUserTwo()
 });
 }
   const handlerRoom = async () => {        
@@ -81,7 +83,9 @@ export default function ChatBox() {
     handlerRoom()
     getUsers()
   }, [idUserTwo])
-
+  const getInfoUserTwo = () => {
+    setUserTwo(user?.find(x => x.id === userIdTwo))
+  }
   return (
     <div>
       {" "}
@@ -91,26 +95,16 @@ export default function ChatBox() {
           <div className="flex-1 p:2 sm:p-6 justify-between flex flex-col h-screen">
             <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
               <div className="relative flex items-center space-x-4">
-                <div className="relative">
-                  {/* <span className="absolute text-green-500 right-0 bottom-0">
+                  <span className="absolute text-green-500 bottom-1  left-2">
                     <svg width={20} height={20}>
                       <circle cx={8} cy={8} r={8} fill="currentColor" />
-                    </svg> */}
-                  {/* </span> */}
-                  {/* <img
-                    src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=144&h=144"
-                    alt=""
-                    className="w-10 sm:w-16 h-10 sm:h-16 rounded-full"
-                  /> */}
-                </div>
-                {/* <div className="flex flex-col leading-tight">
-                  <div className="text-2xl mt-1 flex items-center">
-                    <span className="text-gray-700 mr-3">Anderson Vanhron</span>
-                  </div>
-                  <span className="text-lg text-gray-600">
-                    Junior Developer
+                    </svg>
                   </span>
-                </div> */}
+                <div className="flex flex-col leading-tight">
+                  <div className="text-2xl mt-1 flex items-center">
+                    <span className="text-gray-700 ml-4">{userTwo?.email}</span>
+                  </div>
+                </div>
               </div>
               <div className="flex items-center space-x-2">
                 <button
